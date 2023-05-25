8f4f92fee0f034fc0e83e32f80542cc1b8ef37b2
Swagat <swagatjena0810@gmail.com>
2023-05-25 21:30:22 +0530

    Commit Service Registry

---
+++ ServiceRegistry/src/test/java/com/swagat/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swagat.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
