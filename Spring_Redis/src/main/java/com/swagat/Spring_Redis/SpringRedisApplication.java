a5e6ae6723366410db38e625d5d4318ee8e222fb
Swagat <swagatjena0810@gmail.com>
2023-05-26 11:38:13 +0530

    commit Spring Boot Redis connection app

---
+++ Spring_Redis/src/main/java/com/swagat/Spring_Redis/SpringRedisApplication.java
@@ -0,0 +1,16 @@
+package com.swagat.Spring_Redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringRedisApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisApplication.class, args);
+	
+	}
+
+}
---
+++ Spring_Redis/src/test/java/com/swagat/Spring_Redis/SpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swagat.Spring_Redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
