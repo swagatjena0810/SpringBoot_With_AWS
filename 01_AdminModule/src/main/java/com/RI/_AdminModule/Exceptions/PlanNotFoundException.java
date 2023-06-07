1e342df4c7b7c7bddd80969fc6117ca78706be0e
swagatjena0810 <125973562+swagatjena0810@users.noreply.github.com>
2023-06-07 18:14:49 +0530

    Delete 01_AdminModule directory

--- 01_AdminModule/src/main/java/com/RI/_AdminModule/Exceptions/PlanNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.RI._AdminModule.Exceptions;
-
-public class PlanNotFoundException extends RuntimeException{
-	
-	private static final long serialVersionUID = 1L;
-	public PlanNotFoundException(String msg) {
-		super(msg);
-	}
-}
